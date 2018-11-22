5c71d65bed0eda5e7d2d694d213259e78e55486f
Erik <erikwaskiewicz@gmail.com>
2018-11-22 14:47:43 +0000

    Added function to get LRG exon labels, start and end positions, and return as dictionary

--- comfy_BED.py
+++ comfy_BED.py
@@ -1,6 +1,7 @@
 import argparse
 import textwrap
 import os
+import xml.etree.ElementTree as ET
 
 
 # load arguments
@@ -36,24 +37,48 @@ def get_args():
 
     return parser.parse_args()
 
+def getLrgExons(transcript, lrg_id):
+    '''
+    Make a dictionary (key is string exon label, value is tuple of 'start' and 'end' 
+    positions as integers)
+    '''
+    transcript_dict = {}
+    for exon in transcript.iter('exon'):
+        name = 'exon_{}'.format(exon.get('label'))
+        #from coordinates
+        for coordinate in exon.iter('coordinates'):
+            if coordinate.get('coord_system') == lrg_id:
+                start = int(coordinate.get('start'))
+                end = int(coordinate.get('end'))
+        transcript_dict[name] = (start, end)
+    return(transcript_dict)
 
-
-# create xml element tree object
-  # test that file is an lrg (root.tag)
-
-# extract transcripts - for each transcript:
-  # extract the exon boundries - lrg numbering - make into python dict
-  # extract chr, start, end, strand from mapping region of xml
-  # calculate genomic coordinates, depending on stand orientation
-    # get strand: 1 or -1
-    # add 5000 for 5' or 2000 for 3'
-    # add/subtract overall genomic coords to exon numbers (depending on strand)
-  # output in tab delimted text file - chr, start, end, exon_no (.bed)
 
 def main():
     args = get_args()
     assert os.path.isfile(args.input_LRG), 'The input is not a file.'
     assert args.input_LRG.endswith('.xml'), 'The input file is not an xml file.'
+
+    # create xml element tree object
+    # test that file is an lrg (root.tag)
+    tree = ET.parse(os.path.abspath(args.input_LRG))
+    root = tree.getroot()
+    assert root.tag.upper() == "LRG", 'The input file is not an LRG file'
+    for levels in root.iter('fixed_annotation'):
+        lrg_id = levels.find('id').text
+    # extract the exon boundries - lrg numbering - make into python dict
+    for transcript in root.iter('transcript'):
+        if str(transcript.get('name')) in args.transcripts:
+            transcript_dict = getLrgExons(transcript, lrg_id)
+            print(transcript_dict)
+
+    # extract chr, start, end, strand from mapping region of xml
+    # calculate genomic coordinates, depending on stand orientation
+        # get strand: 1 or -1
+        # add 5000 for 5' or 2000 for 3'
+        # add/subtract overall genomic coords to exon numbers (depending on strand)
+    # output in tab delimted text file - chr, start, end, exon_no (.bed)
+
 
 if __name__ == '__main__':
     main()
