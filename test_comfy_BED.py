0204073eef3dc016077c76c037b78eb37370d710
Erik <erikwaskiewicz@gmail.com>
2018-11-23 14:24:30 +0000

    Additions made to test_comfy_bed for function getGenomeMapping - catching expected errors for when input values are missing from xml

--- test_comfy_BED.py
+++ test_comfy_BED.py
@@ -16,13 +16,17 @@ def test_getGenomeMapping():
     #two genome_builds
     tree_LRG_293 = ET.parse(os.path.abspath("LRG_293.xml"))
     root_LRG_293 = tree_LRG_293.getroot()
+    tree_LRG_293_bad = ET.parse(os.path.abspath("LRG_293_bad.xml"))
+    root_LRG_293_bad = tree_LRG_293_bad.getroot()
     tree_LRG_5 = ET.parse(os.path.abspath("LRG_5.xml"))
     root_LRG_5 = tree_LRG_5.getroot()
 
     assert getGenomeMapping(root_LRG_293, 'GRCh37') == ('chr13', 32884617, 32975809, '1'), "Failed to obtain mapping information"
     assert getGenomeMapping(root_LRG_293, 'GRCh38') == ('chr13', 32310480, 32401672, '1'), "Failed to obtain mapping information"
-    assert getGenomeMapping(root_LRG_293, 'GRCh37') == ('chr13', 1, 1, '1'), "TEST DATA INCORRECT: Failed to obtain mapping information"
-    assert getGenomeMapping(root_LRG_293, 'GRCh38') == ('chr13', 1, 1, '1'), "TEST DATA INCORRECT: Failed to obtain mapping information"
+    with pytest.raises(UnboundLocalError):
+        getGenomeMapping(root_LRG_293_bad, 'GRCh37')
+    with pytest.raises(UnboundLocalError):
+        getGenomeMapping(root_LRG_293_bad, 'GRCh38')
     #assert getGenomeMapping(root_LRG_5, 'GRCh37') ==
     #assert getGenomeMapping(root_LRG_5, 'GRCh38') ==
     #it outputs these: chr, genome_start, genome_end, genome_strand
