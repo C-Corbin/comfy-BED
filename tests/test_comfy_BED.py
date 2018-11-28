bcb8b7d5d39d6fcceebc55c71b7310e0f7f258b1
erikwaskiewicz <32075672+erikwaskiewicz@users.noreply.github.com>
2018-11-28 16:06:55 +0000

    Moved comfy_BED.py and test_confy_BED.py into the comfy_BED/ or tests/ folder, respectively, editted test_comfy_BED.py so that test data filepaths and import statements are correct

--- tests/test_comfy_BED.py
+++ tests/test_comfy_BED.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 
-from comfy_BED import *
+from comfy_BED.comfy_BED import *
 
 '''
 def test_get_args(self):
@@ -14,11 +14,11 @@ def test_getGenomeMapping():
     #make the root for test sets
     #use one 5->3 and one 3->5 set
     #two genome_builds
-    tree_LRG_293 = ET.parse(os.path.abspath("LRG_293.xml"))
+    tree_LRG_293 = ET.parse(os.path.abspath("tests/test_data/LRG_293.xml"))
     root_LRG_293 = tree_LRG_293.getroot()
-    tree_LRG_293_bad = ET.parse(os.path.abspath("LRG_293_bad.xml"))
+    tree_LRG_293_bad = ET.parse(os.path.abspath("tests/test_data/LRG_293_bad.xml"))
     root_LRG_293_bad = tree_LRG_293_bad.getroot()
-    tree_LRG_5 = ET.parse(os.path.abspath("LRG_5.xml"))
+    tree_LRG_5 = ET.parse(os.path.abspath("tests/test_data/LRG_5.xml"))
     root_LRG_5 = tree_LRG_5.getroot()
 
     assert getGenomeMapping(root_LRG_293, 'GRCh37') == ('chr13', 32884617, 32975809, '1'), "Failed to obtain mapping information"
