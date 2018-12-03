783f5889f0b66aa816539f4a061f5f054d1bc1da
erikwaskiewicz <32075672+erikwaskiewicz@users.noreply.github.com>
2018-12-03 12:49:36 +0000

    Made tests for getLrgId function

---
+++ tests/test_web.py
@@ -0,0 +1,36 @@
+import pytest
+import os
+
+from comfy_BED.web import getLrgId
+
+def test_getLrgId():
+    # lrg ID and HGNC gene name
+    assert getLrgId('LRG_1') == 'LRG_1'
+    assert getLrgId('COL1A1') == 'LRG_1'
+
+    # ensembl transcripts
+    assert getLrgId('ENSG00000108821') == 'LRG_1'
+    assert getLrgId('ENSP00000225964.5') == 'LRG_1'
+    assert getLrgId('ENST00000225964.9') == 'LRG_1'
+
+    # refseq transcripts
+    assert getLrgId('NM_000088.3') == 'LRG_1'
+    assert getLrgId('NG_007400.1') == 'LRG_1'
+    assert getLrgId('NP_000079.2') == 'LRG_1'
+
+    # Invalid inputs should throw an assertion error or value error
+    with pytest.raises((ValueError, AssertionError)):
+        getLrgId('invalid_input')
+
+
+def test_CheckLrgExists():
+    pass
+
+def test_getLrgStatus():
+    pass
+
+def test_getLrgXml():
+    pass
+
+def test_getLrgWeb():
+    pass
