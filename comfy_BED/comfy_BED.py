b90e766559595419c08a20dd4762087bc2eb1ce0
C-Corbin <31924492+c-corbin@users.noreply.github.com>
2018-12-04 14:50:30 +0000

    Made function, checkCurrentLrgStatus, which gets the public/pending status of the LRG_ID from the LRG webservices. Returns a LRG status string, and an informational string on how to interpret it. These go in the log and at the header of the BED file.

--- comfy_BED/comfy_BED.py
+++ comfy_BED/comfy_BED.py
@@ -6,6 +6,8 @@ import xml.etree.ElementTree as ET
 import datetime
 import six
 import logging
+import requests
+import json
 
 # load arguments
 def getArgs():
@@ -59,6 +61,39 @@ def setUpLogs(args, now):
     '''
     log_filename = now.strftime("%Y-%m-%d") + "_comfy_BED" + ".log"
     logging.basicConfig(filename=log_filename, level=logging.DEBUG)
+
+
+def checkCurrentLrgStatus(lrg_id):
+    '''
+    Checks the CURRENT status of the user-provided LRG ID on LRG website, returns information to the BED header and log file
+    'Public' LRGs have a 'fixed annotation' section which has been fully finalised
+    'Pending' LRGs do NOT have a finalised 'fixed annotation' section
+    WARNING: User-provided XMLs contain no information as to whether they were public or private at time of download
+    The end user should always download their XMLs very shortly before use
+    '''
+    #get data from webservice
+    url_p1 = "https://www.ebi.ac.uk/ebisearch/ws/rest/lrg/entry/"
+    url_p3 = "?fields=status&format=json"
+    url_full = url_p1 + str(lrg_id) + url_p3
+    logging.info("Checking status with webservice: " + str(url_full))
+    data_return = requests.get(url_full)
+    parsed_data_return = data_return.json()
+
+    # parse the returned data, return status and log message
+    lrg_status_return = parsed_data_return['entries'][0]['fields']['status'][0]
+    if lrg_status_return == "public":
+        lrg_status_message = "The LRG is currently marked 'public' on the LRG website: note that the user-provided file could have been downloaded before the LRG going public"
+    else:
+        if lrg_status_return == "pending":
+            lrg_status_message = "The LRG is currently marked 'pending' on the LRG website: the fixed annotation is not yet finalised, so it should be interpreted with caution"
+    assert (lrg_status_return == "public") or (lrg_status_return == "pending"), "The LRG status could not be resolved as public or private"
+    if (lrg_status_return != "public") and (lrg_status_return != "pending"):
+        logging.error("The LRG status could not be resolved as public or pending")
+        lrg_status_message = "ERROR: The LRG status could not be resolved with the webservice as public or pending"
+
+    logging.info("LRG status is: " + lrg_status_return)
+    logging.info(lrg_status_message)
+    return lrg_status_return, lrg_status_message
 
 
 def getLrgExons(transcript, lrg_id):
@@ -151,12 +186,13 @@ def calculateGenomicPositions(transcript_dict, chrom, gen_start, gen_end, strand
     return list_of_exons
 
 
-def writeToFile(data_list, file_name, now):
+def writeToFile(lrg_status, lrg_status_message, data_list, file_name, now):
     '''
     Take a list of tuples and look through and write as a tab seperated file
     '''
     with open(file_name, 'wb') as out:
         out.writelines('#BED file generated at: ' + now.strftime("%Y-%m-%d %H:%M") + '\n')
+        out.writelines('#' + str(lrg_status) + ": " + str(lrg_status_message) + '\n')
         writer = csv.writer(out, delimiter='\t')
         for row in data_list:
             writer.writerow(row)
@@ -192,6 +228,10 @@ def main():
         lrg_id = levels.find('id').text
         logging.info("LRG_ID: " + lrg_id)
 
+    #check whether this LRG ID is public or pending *as of the time of running*, throw warning if pending
+    #note that a user-provided LRG could be downloaded while pending, then checked later in time when public
+    publicOrPrivate, publicOrPrivateMessage = checkCurrentLrgStatus(lrg_id)
+
     # extract chr, start, end, strand from mapping region of xml
     logging.info("Genome build: " + str(args.genome_build))
     chrom, genome_start, genome_end, genome_strand = getGenomeMapping(root, args.genome_build)
@@ -210,7 +250,7 @@ def main():
             # output in tab delimted text file
             #TODO add header, option to change filename, sorting
             file_name = '{}_{}.bed'.format(lrg_id, transcript_name)
-            writeToFile(exon_genomic_positions, file_name, now)
+            writeToFile(publicOrPrivate, publicOrPrivateMessage, exon_genomic_positions, file_name, now)
             logging.info("Completed BED production for transcript: " + transcript_name)
     logging.info("comfy_BED run complete")
 
